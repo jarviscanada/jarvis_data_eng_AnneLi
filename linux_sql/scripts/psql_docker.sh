438b5b49a7a5919cf48bc47811fd755f14d4f8bb
AnneLi <li.ne.li@ryerson.ca>
2021-01-15 19:53:50 +0000

    psql_docker bug fixes

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -12,29 +12,25 @@ sudo systemctl status docker || systemctl start docker
 case "$command" in
   "create" )
     #check if the container is already created
-    if [ $(docker container ls -a -f name=jrvs-psql | wc -1) -eq 2 ]; then
-      echo "Error: The docker container jrvs-psql has already been created. Check it's usage below:"
+    if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" -eq 2 ]; then
+      echo "The docker container jrvs-psql has already been created. Check it's usage below:"
       docker container ls -a -f name=jrvs-psql
       exit 1
     fi
     #check if the `db_username` or `db_password` is passed through CLI arguments
-    if [ "$#" -ne 3 ]; then
-      echo "Illegal number of parameters. Might be missing db_username and/or db_password"
+    if [ -z $db_database ] || [ -z $db_password ]; then
+      echo "Illegal number of parameters. Might be missing db_username and/or db_password. Correct usage: ./scripts/psql_docker.sh create [db_username][db_password]"
       exit 1
     fi
     #create `pgdata` volume if it does not exist
     docker volume create pgdata
     #create a psql docker container, name it "jrvs-psql"
     docker run --name jrvs-psql -e POSTGRES_PASSWORD=${db_password} -e POSTGRES_USER=${db_username} -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-    if [ "$?" != 0 ]; then
-      echo "Error: Failed to create psql container. Correct usage: ./scripts/psql_docker.sh create [db_username][db_password]"
-      exit 1
-    fi
-    exit 0
+    exit $?
   ;;
 
   "start" )
-    if [ $(docker container ls -a -f name=jrvs-psql | wc -1) -ne 2]; then
+    if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" -ne 2 ]; then
       echo "Error: Docker container does not exist yet. Correct usage: ./scripts/psql_docker.sh create [db_username][db_password]"
       exit 1
     fi
@@ -43,11 +39,11 @@ case "$command" in
   ;;
 
   "stop" )
-    if [ $(docker container ls -a -f name=jrvs-psql | wc -1) -ne 2]; then
+    if [ "$(docker container ls -a -f name=jrvs-psql | wc -1)" -ne 2 ]; then
       echo "Error: Docker container does not exist yet. Correct usage: ./scripts/psql_docker.sh create [db_username][db_password]"
       exit 1
     fi
-    docker container start jrvs-psql
+    docker container stop jrvs-psql
     exit $?
   ;;
   *)
@@ -55,3 +51,5 @@ case "$command" in
     exit 1
   ;;
 esac
+
+exit 0
