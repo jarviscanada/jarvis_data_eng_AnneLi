84a2bf722feb19e98a36437d0207c8220cf0d978
AnneLi <li.ne.li@ryerson.ca>
2021-01-21 09:20:12 +0000

    init host_info and host_usage

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+'Bash script for collecting hardware specification data and then insert the data to the psql instance.
+You can assume that hardware specifications are static, so the script will be executed only once.
+Usage:
+./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password
+Example:
+./scripts/host_info.sh "localhost" 5432 "host_agent" "postgres" "mypassword" '
+
+#Assign CLI arguments to variables
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#This usage info will pop up every time the user provides wrong or missing arguments
+usage="Usage: scripts/host_usage.sh psql_host psql_port db_name psql_user psql_password"
+#Validate arguments
+if [ "$#" ne 5 ]; then
+    echo "Error: illegal number of arguments. Correct usage: $usage"
+    exit 1
+fi
+
+#Set password to an environment variable
+export PGPASSWORD=$psql_password
+
+#Parse host hardware specification
+hostname=$(hostname -f)
+cpu_number=$(lscpu | awk '/CPU\(s\)/{print $2; exit}')
+cpu_architecture=$(lscpu | awk '/^Arch/{print $2; exit}')
+cpu_model=$(lscpu | awk '/^Model/{print $2; exit}')
+cpu_mhz=$(lscpu | awk '/^CPU MHz:/{print $3; exit}')
+L2_cache=$(lscpu | awk '/^L2 cache:/{print $3; exit}')
+total_mem=$(cat /proc/meminfo | awk '/^MemTotal:/{print $2 " " $3; exit}')
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+
+#Insert data into PSQL from Bash script
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, timestamp)
+VALUES ('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$L2_cache', '$total_mem', '$timestamp');"
+
+#Execute the insert statement
+psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$insert_stmt"
---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+'This is a bash script for collecting usage information to store in database
+Script usage:
+bash scripts/host_usage.sh psql_host psql_port db_name psql_user psql_password
+Example:
+bash scripts/host_usage.sh localhost 5432 host_agent postgres password '
+
+#Assign CLI arguments to variables
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#This usage info will pop up every time the user provides wrong or missing arguments
+usage="Usage: scripts/host_usage.sh psql_host psql_port db_name psql_user psql_password"
+#Validate arguments
+if [ "$#" ne 5 ]; then
+    echo "Error: illegal number of arguments. Correct usage: $usage"
+    exit 1
+fi
+
+#Parse server CPU and memory usage data and assign them to meaningful variables
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+hostname="$(hostname -f)"
+memory_free=$(cat /proc/meminfo | awk '/^MemFree/{print $2 " " $3; exit}')
+cpu_idle=$(vmstat | awk '/^ [0-9]/{print $15; exit}')
+cpu_kernel=$(vmstat | awk '/^ [0-9]/{print $14; exit}')
+disk_io=$(vmstat -d | awk '/^sda/{print "cur: " $10 " sec: " $11; exit}')
+disk_available=$(df -BM / | awk '/dev/{print $4; exit}')
+
+#construct the INSERT statement. (Use a subquery to get id by hostname)
+insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+VALUES ('$timestamp', (SELECT id FROM host_info WHERE host_info.hostname = '$hostname'), '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available');"
+
+export PGPASSWORD="$psql_password"
+
+#execute the INSERT statement
+psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$statement"
+exit $?
