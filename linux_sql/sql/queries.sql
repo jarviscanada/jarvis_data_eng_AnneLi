facd2a861547d1f3d93a291613d5b986ffe54de3
AnneLi <li.ne.li@ryerson.ca>
2021-01-26 07:04:23 +0000

    update queries.sql

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -12,10 +12,16 @@ END;
 $$
     LANGUAGE PLPGSQL;
 
-SELECT host_id, host_name, round5(host_usage.timestamp) AS timestamp,
-    AVG((total_mem - memory_free)*100/total_mem) AS avg_used_mem_percent
-    FROM host_usage
-        INNER JOIN host_info
-        ON host_info.id = host_usage.host_id
-        GROUP BY host_id, timestamp
+SELECT host_id, hostname, round5(host_usage.timestamp) AS timestamp_5,
+       AVG((total_mem - memory_free)*100/total_mem) AS avg_used_mem_percent
+       FROM host_usage
+         INNER JOIN host_info
+            ON host_info.id = host_usage.host_id
+    GROUP BY host_id, timestamp_5, hostname
     ORDER BY host_id;
+
+SELECT host_id, timestamp, round5(timestamp), COUNT(*) as num_of_datapoints
+    FROM host_usage
+    GROUP BY host_id, timestamp, round5(timestamp)
+    HAVING COUNT(*) < 3
+ORDER BY host_id;
